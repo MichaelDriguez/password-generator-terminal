d6d4db15059f362a9337ba2ef48b0fc68580bb84
Michael Rodriguez <168235723+MichaelDriguez@users.noreply.github.com>
2024-06-17 11:45:20 -0500

    Add password generation settings
    So far, it only has one setting that can be modified:
    - Change the length of passwords that get generated (between 1 and 50 characters). By default, 16-character passwords are generated.

--- Generator.java
+++ Generator.java
@@ -1,15 +1,18 @@
 import java.lang.Math;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Generator {
+public class Generator extends MainMenu {
 
     public ArrayList<String> generatedPasswords = new ArrayList<>();
     StringBuilder str = new StringBuilder();
+
+    int passwordLength = 16;
     void generator() {
 
         int randomValue;
 
-        while (str.length() < 16) {
+        while (str.length() < passwordLength) {
 
             randomValue = (int) (Math.random() * (126 + 1 - 50) + 50);
             char character = (char) randomValue;
@@ -24,7 +27,7 @@ public class Generator {
         System.out.println("The generated password is " + str);
         generatedPasswords.add(str.toString());
 
-        str.delete(0,16);
+        str.delete(0,passwordLength);
     }
 
     void history() {
@@ -38,7 +41,31 @@ public class Generator {
             System.out.println(i + ". " + str);
             i++;
         }
+    }
 
-        i = 1;
+    void displaySettings() {
+        System.out.println("\n---------------------------");
+        System.out.println("         Settings          ");
+        System.out.println("---------------------------\n");
+
+        System.out.println("Password length: " + passwordLength + " characters.");
+    }
+
+    void modifySettings() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nTip: A strong password typically has 16 or more characters.\n");
+
+        while (true) {
+            System.out.print("Enter password length (1 - 50): ");
+            passwordLength = input.nextInt();
+
+            if (passwordLength < 1 || passwordLength > 50) {
+                System.out.println("Error: Password length outside of range.");
+            } else {
+                System.out.println("Current settings updated.");
+                break;
+            }
+        }
     }
 }
--- MainMenu.java
+++ MainMenu.java
@@ -6,18 +6,19 @@ public class MainMenu {
         Scanner input = new Scanner(System.in);
         Generator genOBJ = new Generator();
         int selection;
+        String modify;
 
         while (true) {
 
             System.out.println("\n---------------------------");
-            System.out.println(" Secure Password Generator");
             System.out.println("         Main Menu         ");
             System.out.println("---------------------------\n");
 
             System.out.println("""
                 1. Generate a password.
-                2. Show previously generated passwords.
-                3. Exit program.
+                2. Show current password generation settings.
+                3. Show previously generated passwords.
+                4. Exit program.
                 """);
 
             System.out.print("Please select an option: ");
@@ -28,14 +29,32 @@ public class MainMenu {
                     genOBJ.generator();
                     break;
                 case 2:
-                    genOBJ.history();
+                    genOBJ.displaySettings();
+
+                    while (true) {
+                        System.out.println("\nDo you wish to modify your settings?");
+                        System.out.print("(Y/N): ");
+                        modify = input.next();
+
+                        if (modify.equals("Y") || modify.equals("y")) {
+                            genOBJ.modifySettings();
+                            break;
+                        } else if (modify.equals("N") || modify.equals("n")) {
+                            break;
+                        } else {
+                            System.out.println("Error: Invalid option.");
+                        }
+                    }
                     break;
                 case 3:
+                    genOBJ.history();
+                    break;
+                case 4:
                     System.out.println("Program exited successfully.");
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Invalid option.");
+                    System.out.println("Error: Invalid option.");
                     break;
             }
         }
